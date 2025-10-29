412cc465bf5ff3b88e1f179c276c720be8fc4820
quanlv.56 <quanlv.56@gmail.com>
2025-10-29 16:53:54 +0700

    Lesson 23: Setup Tanstack Query và code logic login page

---
+++ client-restaurant-app/src/queries/useAuth.tsx
@@ -0,0 +1,8 @@
+import { useMutation } from "@tanstack/react-query";
+import authApiRequest from "../apiRequest/auth";
+
+export const useLoginMutation = () => {
+  return useMutation({
+    mutationFn: authApiRequest.login,
+  });
+};
