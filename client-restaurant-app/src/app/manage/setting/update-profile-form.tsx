f81c3e542c5cc2646512544c8fc1be42d692aaab
quanlv.56 <quanlv.56@gmail.com>
2025-11-14 16:43:09 +0700

    Lesson 46: Fix bug useSearchParam khi build và bug avatar ở trang setting

--- client-restaurant-app/src/app/manage/setting/update-profile-form.tsx
+++ client-restaurant-app/src/app/manage/setting/update-profile-form.tsx
@@ -25,7 +25,7 @@ export default function UpdateProfileForm() {
     resolver: zodResolver(UpdateMeBody),
     defaultValues: {
       name: "",
-      avatar: "",
+      avatar: undefined,
     },
   });
 
@@ -43,7 +43,7 @@ export default function UpdateProfileForm() {
       const { name, avatar } = profile;
       form.reset({
         name: name ?? "",
-        avatar: avatar ?? "",
+        avatar: avatar ?? undefined,
       });
     }
   }, [form, profile]);
