6b24d71d0b363b081a855de35847c167dc2da841
quanlv.56 <quanlv.56@gmail.com>
2025-12-05 23:23:25 +0700

    lesson 63: add text to QR code

--- client-restaurant-app/src/components/qrcode-table.tsx
+++ client-restaurant-app/src/components/qrcode-table.tsx
@@ -11,16 +11,39 @@ export default function QRCodeTable({
   tableNumber: number;
   width?: number;
 }) {
+  // Hiện tại: Thư viện QRCode nó sẽ vẽ lên cái thẻ Canvas
+  // Bây giờ: Chúng ta sẽ tạo 1 cái thẻ canvas ảo để thư viện QRCode code nó vẽ QR lên trên đó.
+  // Và chúng ta sẽ edit thẻ canvas thật
+  // Cuối cùng thì chúng ta sẽ đưa cái thẻ canvas ảo chứa QR Code ở trên vào thẻ Canvas thật
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
 
+  useEffect(() => {
+    const canvas = canvasRef.current!;
+    canvas.height = width + 70;
+    canvas.width = width;
+    const canvasContext = canvas.getContext("2d")!;
+    canvasContext.fillStyle = "#fff";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.font = "20px Arial";
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "#000";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
+    const virtualCanvas = document.createElement("canvas");
     QRCode.toCanvas(
-      canvas,
+      virtualCanvas,
       getTableLink({ token, tableNumber }),
       function (error) {
         if (error) console.error(error);
-        console.log("success!");
+        canvasContext.drawImage(virtualCanvas, 0, 0, width, width);
       }
     );
   }, [token, tableNumber]);
